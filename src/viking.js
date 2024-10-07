c30bb8092ee1ab5eb27bc41c1b7ba580d1774242
Tomás Martín <tmartindsgn@gmail.com>
2024-10-07 16:38:06 +0200

    lab done till bonus

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,49 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+  attack() {
+    return this.strength;
+  }
+  receiveDamage(damageAmount) {
+    this.health -= damageAmount;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+
+  receiveDamage(amount) {
+    super.receiveDamage(amount);
+    if (this.health > 0) {
+      return `${this.name} has received ${amount} points of damage`;
+    } else {
+      return `${this.name} has died in act of combat`;
+    }
+  }
+
+  battleCry() {
+    return "Odin Owns You All!";
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage(amount) {
+    super.receiveDamage(amount);
+    if (this.health > 0) {
+      return `A Saxon has received ${amount} points of damage`;
+    } else {
+      return `A Saxon has died in combat`;
+    }
+  }
+}
 
 // War
 class War {}
